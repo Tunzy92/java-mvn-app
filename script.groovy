98a54b30be642f21d336add79843dee6db4819da
Onabanjo Babatunde <onabanjo.temitayo3@gmail.com>
2025-04-24 20:00:07 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,13 +2,12 @@ def buildJar() {
     echo "testing the application..."
     sh 'mvn package'
 }
+
 def buildImage() {
     echo "building docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build . -t tunzy/demo-image:1'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push tunzy/demo-image:1'
+        sh 'docker build . -t tunzy/demo-image:1.1'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push tunzy/demo-image:1.1'
     }
 }
-
-return this 
